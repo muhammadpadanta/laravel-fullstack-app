d783dfc215a4edf38f251c11148c140071af72c8
KChris117 <offelmarbun@gmail.com>
2024-03-13 22:41:03 +0700

    Penambahan dashboard dan List ticket(BETA)

---
+++ resources/views/ListBarang.blade.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html>
+<head>
+  <title>Dashboard</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+<div class="list-item">
+    <div class="bg-black text-center bg-opacity-45">
+        <h1>Ticket List</h1>
+    </div>
+    </div>
+    
+    <div class="col-md-10">
+    </div>    
+
+    <div class="container">
+        <div class="sidebar1">
+
+        </div>
+        <div class="main-content">
+            <h3>1. NOAH</h3>
+            <img src="{{ asset('images/image (2).jpg') }}" style="width: 200px; height:200px;">
+
+            <p>Kualitas band fenomenal yang satu ini memang tak perlu diragukan lagi. Kami berani taruhan, setidaknya ada 5-6 lagu dari mereka yang pasti kalian hafal setiap bait dari liriknya. Lewatkan jika berbicara mengenai penghargaan yang pernah diraih mereka, karena itu bisa menjadi satu tulisan tersendiri.</P>
+            <a href="">Beli Ticket di sini</a>
+
+        </div>
+    </div>
+    <div class="container">
+
+        <div class="sidebar2">
+
+        </div>
+        <div class="main-content">
+            <h3>2. SLANK</h3>
+            <img src="{{ asset('images/image (1).jpg') }}" style="width: 200px; height:200px;">
+
+            <p>Salah satu band legendaris Indonesia, nih. Nama SLANK ternyata terinspirasi dari kawan-kawan mereka yang sering menyebut mereka cowok slengean. Jadi, Slank merupakan kependekan dari kata slenge’an dan menggunakan huruf K biar lebih keren ucap para personil.</P>
+            <a href="">Beli Ticket di sini</a>
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="sidebar3">
+
+        </div>
+        <div class="main-content">
+            <h3>3. Godbless</h3>
+            <img src="{{ asset('images/image (3).jpg') }}" style="width: 200px; height:200px;">
+
+            <p>Godbless merupakan band rock legendaris Indonesia yang lagunya masih berdendang-dendang di telinga penikmat musik rock Indonesia hingga saat ini. Band ini dulunya bernama Crazy Wheels dan sempat mengadakan konser perdananya di Taman Ismail Marzuki serta mengikuti berbagai pentas band di Indonesia, Malaysia dan Filipina. Band yang diawali oleh tangan besar Achmad Albar setelah dirinya kembali ke Indonesia ini berganti nama menjadi God Bless pada tanggal 5 Mei 1973.</P>
+            <a href="">Beli Ticket di sini</a>
+
+        </div>
+    </div>
+    <div class="container">
+
+        <div class="sidebar4">
+
+        </div>
+        <div class="main-content">
+            <h3>4. Mocca</h3>
+            <img src="{{ asset('images/image (4).jpg') }}" style="width: 200px; height:200px;">
+
+            <p>Mocca dibentuk pada tahun 1997 oleh Arina Ephipania (vokal, flute) dan Riko Prayitno (gitar), yang bertemu saat kuliah di Institut Teknologi Nasional. Dalam wawancara tahun 2004 dengan The Straits Times Ephipania mengatakan "Riko dipengaruhi oleh musik rock, saya terpesona oleh musik film dan bercerita, jadi kami datang dengan konsep bercerita dalam bentuk musik." Dua anggota utama lainnya adalah Achmad Pratama (alias Toma) (bass) dan Indra Massad (drum). Mereka menandatangani kontrak dengan label independen Fast Forward Records pada tahun 2002. Band ini populer di Singapura, Malaysia, Thailand, Jepang, Korea dan Taiwan. Penggemar Mocca disebut Swinging Friends.</P>
+            <a href="">Beli Ticket di sini</a>
+
+        </div>
+    </div>
+</body>
+</html>
