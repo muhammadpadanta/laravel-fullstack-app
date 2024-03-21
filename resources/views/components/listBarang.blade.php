190db03ab70d5a5e5de393091a5b4c82fc1d1107
KChris117 <offelmarbun@gmail.com>
2024-03-21 11:28:49 +0700

    Perubahan Ticket List()

--- resources/views/components/listBarang.blade.php
+++ resources/views/components/listBarang.blade.php
@@ -58,68 +58,67 @@
 <nav>
     @include('components.navbar')
 </nav>
-<div class="list-item">
-    <div class="bg-black text-center bg-opacity-45">
-        <h1>Ticket List</h1>
-    </div>
+
+<div class="bg-gray-800 text-white flex justify-center">
+    <div class="p-5 space-y-5">
+    <div class="flex items-start space-x-4 border-4 border-white px-8 py-8 rounded ">
+      <img src="{{ asset('img/foto1.png') }}" style="width: 400px; height:500px;">
+      <div class="space-y-2">
+        <h3 class="text-xl">15 February 2024</h2>
+        <h2 class="text-xl">JAKARTA, INDONESIA ICE BSD CITY</h2>
+        <p>Rp 800.000 - Rp 13.000.000</p>
+      <button class="bg-purple-600 text-white px-4 py-2 rounded">Get tickets</button>
+        <h3 class="text-xl">20 March 2024</h2>
+        <h2 class="text-xl">JAKARTA, INDONESIA ICE BSD CITY</h2>
+        <p>Rp 800.000 - Rp 13.000.000</p>
+      <button class="bg-purple-600 text-white px-4 py-2 rounded">Get tickets</button>
+        <h3 class="text-xl">10 May 2024</h2>
+        <h2 class="text-xl">JAKARTA, INDONESIA ICE BSD CITY</h2>
+        <p>Rp 800.000 - Rp 13.000.000</p>
+      <button class="bg-purple-600 text-white px-4 py-2 rounded">Get tickets</button>
+        </div>
+    <div class="space-y-2">
+        <h3 class="text-xl">25 July 2024</h2>
+        <h2 class="text-xl">JAKARTA, INDONESIA ICE BSD CITY</h2>
+        <p>Rp 800.000 - Rp 13.000.000</p>
+      <button class="bg-purple-600 text-white px-4 py-2 rounded">Get tickets</button>
+        <h3 class="text-xl">05 September 2024</h2>
+        <h2 class="text-xl">JAKARTA, INDONESIA ICE BSD CITY</h2>
+        <p>Rp 800.000 - Rp 13.000.000</p>
+      <button class="bg-purple-600 text-white px-4 py-2 rounded">Get tickets</button>
+      </div>
     </div>
     
-    <div class="col-md-10">
-    </div>    
-
-    <div class="container">
-        <div class="sidebar1">
-
+    <div class="flex items-start space-x-4 border-4 border-white px-8 py-8 rounded ">
+      <img src="{{ asset('img/foto2.png') }}" style="width: 400px; height:500px;">
+      <div class="space-y-2" >
+        <h3 class="text-xl">15 February 2024</h2>
+        <h2 class="text-xl">JAKARTA, INDONESIA ICE BSD CITY</h2>
+        <p>Rp 800.000 - Rp 13.000.000</p>
+      <button class="bg-purple-600 text-white px-4 py-2 rounded">Get tickets</button>
+        <h3 class="text-xl">20 March 2024</h2>
+        <h2 class="text-xl">JAKARTA, INDONESIA ICE BSD CITY</h2>
+        <p>Rp 800.000 - Rp 13.000.000</p>
+      <button class="bg-purple-600 text-white px-4 py-2 rounded">Get tickets</button>
+        <h3 class="text-xl">10 May 2024</h2>
+        <h2 class="text-xl">JAKARTA, INDONESIA ICE BSD CITY</h2>
+        <p>Rp 800.000 - Rp 13.000.000</p>
+      <button class="bg-purple-600 text-white px-4 py-2 rounded">Get tickets</button>
         </div>
-        <div class="main-content">
-            <h3>1. NOAH</h3>
-            <img src="https://cdn0-production-images-kly.akamaized.net/89kuS8x4OJ-kWIUVUFuyjHE-cn4=/1200x900/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/830167/original/091714900_1426578866-noah.jpg" style="width: 200px; height:200px;">
-
-            <p>Kualitas band fenomenal yang satu ini memang tak perlu diragukan lagi. Kami berani taruhan, setidaknya ada 5-6 lagu dari mereka yang pasti kalian hafal setiap bait dari liriknya. Lewatkan jika berbicara mengenai penghargaan yang pernah diraih mereka, karena itu bisa menjadi satu tulisan tersendiri.</P>
-            <a href="">Beli Ticket di sini</a>
-
-        </div>
+    <div class="space-y-2">
+        <h3 class="text-xl">25 July 2024</h2>
+        <h2 class="text-xl">JAKARTA, INDONESIA ICE BSD CITY</h2>
+        <p>Rp 800.000 - Rp 13.000.000</p>
+      <button class="bg-purple-600 text-white px-4 py-2 rounded">Get tickets</button>
+        <h3 class="text-xl">05 September 2024</h2>
+        <h2 class="text-xl">JAKARTA, INDONESIA ICE BSD CITY</h2>
+        <p>Rp 800.000 - Rp 13.000.000</p>
+      <button class="bg-purple-600 text-white px-4 py-2 rounded">Get tickets</button>
+      </div>
     </div>
-    <div class="container">
 
-        <div class="sidebar2">
-
-        </div>
-        <div class="main-content">
-            <h3>2. SLANK</h3>
-            <img src="https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/jawapos/2019/08/Slank-Album-Baru-Dery-Ridwansah-2-1.jpg" style="width: 200px; height:200px;">
-
-            <p>Salah satu band legendaris Indonesia, nih. Nama SLANK ternyata terinspirasi dari kawan-kawan mereka yang sering menyebut mereka cowok slengean. Jadi, Slank merupakan kependekan dari kata slenge’an dan menggunakan huruf K biar lebih keren ucap para personil.</P>
-            <a href="">Beli Ticket di sini</a>
-
-        </div>
     </div>
-    <div class="container">
-        <div class="sidebar3">
-
-        </div>
-        <div class="main-content">
-            <h3>3. Godbless</h3>
-            <img src="https://www.ciputraartpreneur.com/assets/images/105972godbless.jpg" style="width: 200px; height:200px;">
-
-            <p>Godbless merupakan band rock legendaris Indonesia yang lagunya masih berdendang-dendang di telinga penikmat musik rock Indonesia hingga saat ini. Band ini dulunya bernama Crazy Wheels dan sempat mengadakan konser perdananya di Taman Ismail Marzuki serta mengikuti berbagai pentas band di Indonesia, Malaysia dan Filipina. Band yang diawali oleh tangan besar Achmad Albar setelah dirinya kembali ke Indonesia ini berganti nama menjadi God Bless pada tanggal 5 Mei 1973.</P>
-            <a href="">Beli Ticket di sini</a>
-
-        </div>
-    </div>
-    <div class="container">
-
-        <div class="sidebar4">
-
-        </div>
-        <div class="main-content">
-            <h3>4. Mocca</h3>
-            <img src="https://asset-a.grid.id/crop/144x119:1744x1185/x/photo/2019/11/23/3527419216.jpg" style="width: 200px; height:200px;">
-
-            <p>Mocca dibentuk pada tahun 1997 oleh Arina Ephipania (vokal, flute) dan Riko Prayitno (gitar), yang bertemu saat kuliah di Institut Teknologi Nasional. Dalam wawancara tahun 2004 dengan The Straits Times Ephipania mengatakan "Riko dipengaruhi oleh musik rock, saya terpesona oleh musik film dan bercerita, jadi kami datang dengan konsep bercerita dalam bentuk musik." Dua anggota utama lainnya adalah Achmad Pratama (alias Toma) (bass) dan Indra Massad (drum). Mereka menandatangani kontrak dengan label independen Fast Forward Records pada tahun 2002. Band ini populer di Singapura, Malaysia, Thailand, Jepang, Korea dan Taiwan. Penggemar Mocca disebut Swinging Friends.</P>
-            <a href="">Beli Ticket di sini</a>
-
-        </div>
-    </div>
+</div>
+    
 </body>
 </html>
